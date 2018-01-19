404e28b7b9f1ae320748203864db1c1bfea1a14a
Dacharat Pankong <dacharat.p@ku.th>
2018-01-20 05:50:52 +0700

    Add javadoc in MoneyUtil

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -6,6 +6,13 @@ import java.util.List;
 
 public class MoneyUtil {
 
+	/**
+	 * Check money in purse that they have this currency or not if some coin is not remove it.
+	 * 
+	 * @param coins in purse
+	 * @param currency of money
+	 * @return coin with all same currency
+	 */
 	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
 		for(Coin c : coins) {
 			if(c.getCurrency() != currency)
@@ -14,12 +21,22 @@ public class MoneyUtil {
 		return coins;
 	}
 	
+	/**
+	 * print coin in purse
+	 * 
+	 * @param coins
+	 */
 	public static void printList(List<Coin> coins) {
 		for(Coin c: coins) {
 			System.out.println(c);
 		}
 	}
 	
+	/**
+	 * Sort coin that minimum value will come first
+	 * 
+	 * @param coins
+	 */
 	public static void sortCoins(List<Coin> coins) {
 		Collections.sort(coins);
 		printList(coins);
